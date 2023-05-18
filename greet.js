21bc40b24907c875b907bfe5f582b2081db7f619
Thembakazi <thembakazingamlana25@gmail.com>
2023-05-18 13:52:35 +0200

    adding changes

--- greet.js
+++ greet.js
@@ -1,68 +1,122 @@
-var userName = document.querySelector(".textString");
-var languageType = document.querySelectorAll(".lang");
-var greetBtn = document.querySelector(".radiolanguage");
-var myReset = document.querySelector(".refreshBtn");
-var message = document.querySelector(".message");
+function createGreetingApp() {
+  const userName = document.querySelector(".textString");
+  const languageType = document.querySelectorAll(".lang");
+  const greetBtn = document.querySelector(".radiolanguage");
+  const myReset = document.querySelector(".refreshBtn");
+  const message = document.querySelector(".message");
+  const peopleNamesCounter = document.querySelector(".peopleNamesCounter");
+  const counterEl =  document.querySelector(".counter")
+  let peopleNamesCount = getNameCount(); 
 
-var count = 0;
-
-var peopleNamesCount = 0;
-var btn = document.querySelector('button');
-btn.addEventListener('click', function() {
-  peopleNamesCount++;
-  localStorage['namesCount'] = peopleNamesCount;
-  if (localStorage['namesCount']) {
-    peopleNamesCount = Number(localStorage['namesCount']);
+  function incrementPeopleNamesCount() {
+    peopleNamesCount++;
+    localStorage.setItem('namesCount', peopleNamesCount);
+    const greetCountDisplay = document.querySelector('.greetCount');
+    greetCountDisplay.textContent = peopleNamesCount;
   }
-});
 
-function nameCount(username, language) {
-  let totalName = username.toLowerCase().split(',');
-  let greeting = "";
-  for (let i = 0; i < totalName.length; i++) {
-    if (totalName[i].trim().toLowerCase() === "name") {
-      count++;
-    }
-    if (language[i] === "english") {
-      greeting += "Hello " + totalName[i];
-    }
-    if (language[i] === "isixhosa") {
-      greeting += "Molo " + totalName[i];
-    }
-    if (language[i] === "french") {
-      greeting += "Bonjour " + totalName[i];
-    }
+  function resetPeopleNamesCount() {
+    peopleNamesCount = '';
+    localStorage.removeItem('namesCount');
+    const greetCountDisplay = document.querySelector('.greetCount');
+    greetCountDisplay.textContent = peopleNamesCount;
   }
-  return greeting;
-}
-greetBtn.addEventListener('click', function() {
-  const selectedLanguage = document.querySelector('input[name="language"]:checked').value;
-  const selectedName = userName.value;
-  const totalName = selectedName.toLowerCase().split(',');
-  let message = "";
-  for (let i = 0; i < totalName.length; i++) {
-    languageType.forEach(function(language) {
-      if (language.checked) {
-        if (selectedLanguage === "english") {
-          message += "Hello " + totalName[i] + " ";
-        } else if (selectedLanguage === "isixhosa") {
-          message += "Molo " + totalName[i] + " ";
-        } else if (selectedLanguage === "french") {
-          message += "Bonjour " + totalName[i] + " ";
-        }
-      }
+
+  function getNameCount() {
+    const namesCount = localStorage.getItem('namesCount');
+    if (namesCount) {
+     
+      return namesCount++;
+    }
+    return 0;
+  }
+
+  function getGreetingMessage(selectedLanguage, selectedName) {
+    let greeting = '';
+    if (selectedLanguage === 'english') {
+      greeting = `Hello, ${selectedName}`;
+    } else if (selectedLanguage === 'isixhosa') {
+      greeting = `Molo, ${selectedName}`;
+    } else if (selectedLanguage === 'french') {
+      greeting = `Bonjour, ${selectedName}`;
+    }
+    return greeting;
+  }
+
+  function handleGreetBtnClick() {
+    const selectedLanguage = document.querySelector('input[name="language"]:checked');
+    const selectedName = userName.value.trim();
+    if (!selectedName) {
+      message.textContent = 'Please enter a name.';
+      return;
+    }
+    if (!selectedLanguage) {
+      message.textContent = 'Please select a language.';
+      return;
+    }
+    const greetingMessage = getGreetingMessage(selectedLanguage.value, selectedName);
+    message.textContent = greetingMessage;
+    incrementPeopleNamesCount();
+    let namesCount = getNameCount();
+    
+    counterEl.innerHTML = namesCount
+    peopleNamesCounter.textContent = namesCount;
+  }
+
+  function handleMyResetClick() {
+    resetPeopleNamesCount();
+    userName.value = "";
+    languageType.forEach(function (language) {
+      language.checked = false;
     });
+    message.textContent = "";
   }
-  document.querySelector('.peopleNamesCounter').textContent = message;
-});
-myReset.addEventListener('click', function() {
-  count = 0;
-  peopleNamesCount = 0;
-  localStorage.removeItem('namesCount');
-  document.querySelector('.peopleNamesCounter').textContent = "";
-  userName.value = "";
-  languageType.forEach(function(language) {
-    language.checked = false;
-  });
-  message.textContent = "";
-});
+
+  return {
+    handleGreetBtnClick,
+    handleMyResetClick,
+  };
+}
+
+const myGreetingApp = createGreetingApp();
+
+const greetBtn = document.querySelector('.radiolanguage');
+greetBtn.addEventListener('click', myGreetingApp.handleGreetBtnClick);
+
+const resetBtn = document.querySelector('.refreshBtn');
+resetBtn.addEventListener('click', myGreetingApp.handleMyResetClick);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
